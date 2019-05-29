15db120c86c51d71782bfd93084d92cf516fc83a
jaij89 <40306895+jaij89@users.noreply.github.com>
2019-05-28 20:28:36 -0500

    Create server.js

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-var http = require('http');
+var express = require('express');
 
-var server = http.Server();
+var app = express();
 
-server.on('request', function(request, response) {
+app.use(express.static('views'))
+
+app.listen(1337);
+/* var server = http.Server();
+
+server.on('request', function (request, response) {
     response.write('Hello World!');
     response.end();
-} );
+});
 
-server.listen(1337);
+server.listen(1337); */
